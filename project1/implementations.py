5ad0bfc2179483a375f85311337e6078389b85c1
ideriedm <32436526+ideriedm@users.noreply.github.com>
2020-10-01 09:26:57 +0200

    Beginning of project 1 : Higgs boson

---
+++ project1/implementations.py
@@ -0,0 +1,91 @@
+# -*- coding: utf-8 -*-
+"""
+Éditeur de Spyder
+
+Ceci est un script temporaire.
+"""
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+def least_squares_GD(y, tx, initial_w, max_iters, gamma):
+    
+    """Linear regression using gradient descent
+        Should return : (w,loss) 
+        [last w vector + corresponding loss]
+    """
+    # ***************************************************
+    # INSERT YOUR CODE HERE
+    # TODO: Linear regression using gradient descent
+    # ***************************************************
+    
+    raise NotImplementedError
+    
+def least_squares_SGD(y, tx, initial_w, max_iters, gamma):
+    """Linear regression using stochastic gradient descent
+        Should return : (w,loss) 
+        [last w vector + corresponding loss]
+    """
+    # ***************************************************
+    # INSERT YOUR CODE HERE
+    # TODO: Linear regression using stochastic gradient descent
+    # ***************************************************
+    
+    raise NotImplementedError
+
+    
+def least_squares(y, tx):
+    """Least squares regression using normal equations
+        Should return : (w,loss) 
+        [last w vector + corresponding loss]
+    """
+    # ***************************************************
+    # INSERT YOUR CODE HERE
+    # TODO: Least squares regression using normal equations
+    # ***************************************************
+    
+    raise NotImplementedError
+    
+    
+def ridge_regression(y, tx, lambda_):
+    """Ridge regression using normal equations
+        Should return : (w,loss) 
+        [last w vector + corresponding loss]
+    """
+    # ***************************************************
+    # INSERT YOUR CODE HERE
+    # TODO: Ridge regression using normal equations
+    # ***************************************************
+    
+    raise NotImplementedError
+    
+def logistic_regression(y, tx, initial_w, max_iters, gamma):
+    """Logistic regression using gradient descent or SGD
+        Should return : (w,loss) 
+        [last w vector + corresponding loss]
+    """
+    # ***************************************************
+    # INSERT YOUR CODE HERE
+    # TODO: Logistic regression using gradient descent or SGD
+    # ***************************************************
+    
+    raise NotImplementedError
+    
+def reg_logistic_regression(y, tx, lambda_, initial_w, max_iters, gamma):
+    """Regularized logistic regression using gradient descent or SGD
+        Should return : (w,loss) 
+        [last w vector + corresponding loss]
+    """
+    # ***************************************************
+    # INSERT YOUR CODE HERE
+    # TODO: Regularized logistic regression using gradient descent or SGD
+    # ***************************************************
+    
+    raise NotImplementedError
+    
+    
+    
+    
+    
+    
+    
